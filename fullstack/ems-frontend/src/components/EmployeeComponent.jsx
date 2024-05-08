9c754238df650ef910734d4662a8cf669321a553
sakshi dandriyal <sakshidandriyal377@gmail.com>
2024-05-08 22:37:36 +0530

    added ems frontend code

--- fullstack/ems-frontend/src/components/EmployeeComponent.jsx
+++ fullstack/ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
-import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
 
     const [firstName,setFirstName] = useState('')
     const [lastName,setLastName] = useState('')
     const [email,setEmail] = useState('')
+
+    const {id} = useParams();
 
     const [errors,setErrors] = useState({
         firstName:'',
@@ -21,16 +23,41 @@ const EmployeeComponent = () => {
 
     const navigator = useNavigate();
 
-    function saveEmployee(e){
+    useEffect(() => {
+        if(id){
+            getEmployee(id).then(
+                (response) => {
+                    setFirstName(response.data.firstName);
+                    setLastName(response.data.lastName);
+                    setEmail(response.data.email);
+                }).catch(error => {
+                    console.log(error);
+                })
+        }
+    },[id])
+
+    function saveOrUpadteEmployee(e){
         e.preventDefault();
         const employee = {firstName,lastName,email}
         console.log(employee)
+
         if(validateForm()){
-            createEmployee(employee).then( 
-                (response) => {
-                console.log(response.data);
-                navigator('/employees')
-            })
+                if(id){
+                    updateEmployee(id,employee).then((response) => {
+                        console.log(response.data);
+                        navigator('/employees');
+                    }).catch(error => {
+                        console.log(error);
+                    })
+                } else {
+                    createEmployee(employee).then( 
+                        (response) => {
+                        console.log(response.data);
+                        navigator('/employees')
+                    }).catch(error => {
+                        console.log(error);
+                })
+            }
         }
     }
 
@@ -64,12 +91,22 @@ const EmployeeComponent = () => {
         return valid;
     }
 
+    function pageTitle(){
+         if(id){
+            return <h2 className='text-center'>Update Employee</h2>
+         }else{
+            return <h2 className='text-center'>Add Employee</h2>
+         }
+    }
+
   return (
     <div className='container'>
     <br></br>
         <div className='row'>
             <div className='card col-mb-6 offset-md-3 offset-md-3'>
-                <h2 className='text-center'>Add Employee</h2>
+                {
+                  pageTitle  
+                }
                 <div className='card-body'>
                     <form>
                         <div className='form-group mb-2'>
@@ -111,7 +148,7 @@ const EmployeeComponent = () => {
                             </input>
                             {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
                         </div>
-                        <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                        <button className='btn btn-success' onClick={saveOrUpadteEmployee}>Submit</button>
                     </form>
                 </div>
             </div>
--- fullstack/ems-frontend/src/components/ListEmployeeComponent.jsx
+++ fullstack/ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect, useState} from 'react'
-import { listEmployees } from '../services/EmployeeService';
+import { listEmployees, deleteEmployee } from '../services/EmployeeService';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -9,19 +9,33 @@ const ListEmployeeComponent = () => {
     const [employees,setEmployees] = useState([]);
     const navigator = useNavigate();
 
-    useEffect(() => {
+    useEffect(() => {getAllEmployees();}, [])
+
+    function getAllEmployees(){
         listEmployees().then((response) => {
             setEmployees(response.data)
         }).catch(error => {
             console.log(error);
         })
-    }, [])
-
+    }
 
     function addNewEmployee(){
         navigator('/add-employee')
     }
 
+    function updateEmployee(id){
+        //backtick ` `
+        navigator(`/edit-employee/${id}`)
+    }
+
+    function removeEmployee(id){
+        console.log(id);
+        deleteEmployee(id).then((response) => {
+            getAllEmployees();
+        }).catch(error => {
+            console.log(error);
+        })
+    }
 
   return (
     <div className='container'>
@@ -34,6 +48,7 @@ const ListEmployeeComponent = () => {
                     <th>Employee First Name</th>
                     <th>Employee Last Name</th>
                     <th>Employee email Id </th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -44,6 +59,10 @@ const ListEmployeeComponent = () => {
                         <td>{employee.firstName}</td>
                         <td>{employee.lastName}</td>
                         <td>{employee.email}</td>
+                        <td>
+                            <button className='btn btn-info' onClick = {() => updateEmployee(employee.id)}>Update</button>
+                            <button className='btn btn-danger' onClick = {() => removeEmployee(employee.id)} style={{marginLeft :'10px'}}>Delete</button>
+                        </td>
                     </tr>)
                 }
             </tbody>
