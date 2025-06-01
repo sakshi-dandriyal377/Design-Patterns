ef9b0bb887c309783628714c563ea183004cb7f5
sakshi dandriyal <sakshidandriyal377@gmail.com>
2025-06-01 22:34:51 +0530

    modify decorator pattern

--- design-patterns/src/structural/decorator/Main.java
+++ design-patterns/src/structural/decorator/Main.java
@@ -7,5 +7,12 @@ public class Main {
         Pizza pizza = new PeppyPaneer(new ExtraCheese(new PlainPizza()));
 
         System.out.println(pizza.getDescription()+"="+pizza.getCost());
+
+        Pizza pizza1 = new PlainPizza();
+        pizza1 = new ExtraCheese(pizza1);
+        //pizza1 = new PeppyPaneer(pizza1);
+
+        System.out.println(pizza1.getDescription()+"="+pizza1.getCost());
+
     }
 }
